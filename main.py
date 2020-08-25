66cb4c92d2c972d3803227e607ea203de0d519f8
Karthik Mahadevan <karthik.mahadevan@ucalgary.ca>
2020-08-25 19:40:00 +0000

    Added variables about score

--- main.py
+++ main.py
@@ -5,3 +5,8 @@ t = turtle.Screen()
 t.title("Ping Pong Game")
 t.bgcolor("Black")
 t.setup(width = 700, height = 500)
+
+# Player scores
+paddle_one_score = 0
+paddle_two_score = 0
+SCORE_TO_WIN = 5
